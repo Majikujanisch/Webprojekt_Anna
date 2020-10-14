27f96afa1c69721443a91c6be62cb37590c32e8f
Majikujanisch <44255546+Majikujanisch@users.noreply.github.com>
2020-10-14 16:22:42 +0200

    create Project

---
+++ Webprojekt_Anna/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Webprojekt_Anna.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
