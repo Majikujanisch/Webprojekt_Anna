52c17a937d2919addca418b7d88a5b7321f24ee2
Majikujanisch <q1KGy6d8AaS0>
2020-10-21 13:10:55 +0200

    Navigation started

--- Webprojekt_Anna/Controllers/Pokemon.cs
+++ Webprojekt_Anna/Controllers/Pokemon.cs
@@ -1,20 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace Webprojekt_Anna.Controllers
 {
-    public class HomeController : Controller
+    public class Pokemon : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
-        
-
     }
 }
