e550368ee3091f177c31c60296bc99b7b981e372
Majikujanisch <q1KGy6d8AaS0>
2020-10-21 15:29:20 +0200

    Show of all pokemon in a list complet

--- Webprojekt_Anna/Controllers/PokemonController.cs
+++ Webprojekt_Anna/Controllers/PokemonController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Webprojekt_Anna.Models;
 
 namespace Webprojekt_Anna.Controllers
 {
-    public class Pokemon : Controller
+    public class PokemonController : Controller
     {
         public IActionResult Index()
         {
-            return View();
+            return View(CreatePokemonList());
         }
+        private List<Pokemon> CreatePokemonList()
+    {
+        return new List<Pokemon>()
+        {
+            new Pokemon(1, "Bisasam", "Planze", "Gift", 0.7),
+            new Pokemon(2, "Bisaknosp", "Planze", "Gift", 1.0),
+            new Pokemon(3, "Bisaflor", "Planze", "Gift", 2.0),
+        };
     }
+    }
+
+
+    
 }
--- Webprojekt_Anna/Models/Pokemon.cs
+++ Webprojekt_Anna/Models/Pokemon.cs
@@ -7,26 +7,24 @@ namespace Webprojekt_Anna.Models
         public int Pokedex { get; set; }
 
         public string Name{ get; set; }
-        public string Brand { get; set; }
         public string Typ1 { get; set; }
         public string Typ2 { get; set; }
         public double Größe { get; set; }
-        public Pokemon() : this(0,"","","","",0.0m) { }
+        public Pokemon() : this(0,"","","",0.0) { }
 
-        public Pokemon(int id, string name, string brand, DateTime release,decimal price, string cate)
+        public Pokemon(int id, string name, string typ1, string typ2, double größe)
         {
-            this.ID = id;
-            this.ArticleName = name;
-            this.Brand = brand;
-            this.ReleaseDate = release;
-            this.Price = price;
-            this.Category = cate;
+            this.Pokedex = id;
+            this.Name = name;
+            this.Typ1 = typ1;
+            this.Typ2 = typ2;
+            this.Größe = größe;
 
         }
 
         public override string ToString()
         {
-            return ID + " " + ArticleName + " " + Brand + " " + ReleaseDate + " " + Price + " Euro" + Category;
+            return Pokedex + " " + Name + " " + Typ1 + " " + Typ2 + " " + Größe + " m";
         }
 
 
